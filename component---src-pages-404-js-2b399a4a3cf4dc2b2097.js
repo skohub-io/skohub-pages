0c840d001066c975e95dbe3deaafc98869e4682d
sroertgen <sroertgen@users.noreply.github.com>
2023-04-04 14:49:44 +0000

    deploy: f4497d1fc4d1f61b6430006a475d01d01293086e

--- component---src-pages-404-js-2b399a4a3cf4dc2b2097.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkskohub_vocabs=self.webpackChunkskohub_vocabs||[]).push([[883],{429:function(t,e,s){s.r(e);s(7294);var u=s(4110),n=s(262),a=s(213);e.default=function(){return(0,a.tZ)(u.Z,null,(0,a.tZ)("div",{className:"centerPage"},(0,a.tZ)(n.Z,{title:"404: Not found"}),(0,a.tZ)("h1",null,"NOT FOUND"),(0,a.tZ)("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-2b399a4a3cf4dc2b2097.js.map
